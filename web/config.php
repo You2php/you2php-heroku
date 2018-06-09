d838fbbcbdd24967eaea51c4019000bf90cba179
Adobephotoshare <40122556+Adobephotoshare@users.noreply.github.com>
2018-06-10 00:50:15 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
+define('APIKEY', 'AIzaSyAypuEkgLe_wBTplH9VMGWXEHZIn6tWL7g');//这里改成您的api key
 define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('SITE_NAME', 'Y2');//网站名字
+define('TITLENAME', 'Y2');//网站名字
+define('EN2DEKEY', '1q2w3e4r5t');//一个随机字符串，加解密KEY
+define('EMAIL', 'adobephotoshare@gmail.com');//邮箱
 ?>
